564bf582961f777721d7221a425cc526b7f9fc82
mervo <9879403+mervo@users.noreply.github.com>
2020-11-11 11:21:14 +0800

    Working Part 3 FK.

--- HW1/Kinematics/Locobot.py
+++ HW1/Kinematics/Locobot.py
@@ -6,134 +6,136 @@ import RobotUtil as rt
 import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
 
+
 class Locobot:
 
-	def __init__(self):
-		# Robot descriptor taken from URDF file (rpy xyz for each rigid link transform) - NOTE: don't change
-		self.Rdesc=[
-			[0, 0, 0, 0.0973, 0, 0.1730625], # From robot base to joint1
-			[0, 0, 0, 0, 0, 0.04125],
-			[0, 0, 0, 0.05, 0, 0.2],
-			[0, 0, 0, 0.2002, 0, 0],
-			[0, 0, 0, 0.063, 0.0001, 0],
-			[0, 0, 0, 0.106525, 0, 0.0050143] # From joint5 to end-effector center
-			]
+    def __init__(self):
+        # Robot descriptor taken from URDF file (rpy xyz for each rigid link transform) - NOTE: don't change
+        self.Rdesc = [
+            [0, 0, 0, 0.0973, 0, 0.1730625],  # From robot base to joint1
+            [0, 0, 0, 0, 0, 0.04125],
+            [0, 0, 0, 0.05, 0, 0.2],
+            [0, 0, 0, 0.2002, 0, 0],
+            [0, 0, 0, 0.063, 0.0001, 0],
+            [0, 0, 0, 0.106525, 0, 0.0050143]  # From joint5 to end-effector center
+        ]
 
-		# TODO: Define the axis of rotation for each joint
-		'''Note: you will get this from the URDF (interbotix_locobot_description.urdf).
-		For example, in the urdf under each joint you will see: (<axis xyz="0 1 0"/>)
-		'''
-		self.axis= ([[0,0,1],
-					 [0,1,0],
-					 [0,1,0],
-					 [0,1,0],
-					 [-1,0,0],
-					 [0,1,0]
-					 ])
+        # TODO: Define the axis of rotation for each joint
+        '''Note: you will get this from the URDF (interbotix_locobot_description.urdf).
+        For example, in the urdf under each joint you will see: (<axis xyz="0 1 0"/>)
+        '''
+        self.axis = ([[0, 0, 1],
+                      [0, 1, 0],
+                      [0, 1, 0],
+                      [0, 1, 0],
+                      [-1, 0, 0],
+                      [0, 1, 0]
+                      ])
 
-		#Set base coordinate frame as identity - NOTE: don't change
-		self.Tbase= [[1,0,0,0],
-			[0,1,0,0],
-			[0,0,1,0],
-			[0,0,0,1]]
+        # Set base coordinate frame as identity - NOTE: don't change
+        self.Tbase = [[1, 0, 0, 0],
+                      [0, 1, 0, 0],
+                      [0, 0, 1, 0],
+                      [0, 0, 0, 1]]
 
-		#Initialize matrices - NOTE: don't change this part
-		self.Tlink=[] #Transforms for each link (const)
-		self.Tjoint=[] #Transforms for each joint (init eye)
-		self.Tcurr=[] #Coordinate frame of current (init eye)
-		for i in range(len(self.Rdesc)):
-			self.Tlink.append(rt.rpyxyz2H(self.Rdesc[i][0:3],self.Rdesc[i][3:6]))
-			self.Tcurr.append([[1,0,0,0],[0,1,0,0],[0,0,1,0.],[0,0,0,1]])
-			self.Tjoint.append([[1,0,0,0],[0,1,0,0],[0,0,1,0.],[0,0,0,1]])
+        # Initialize matrices - NOTE: don't change this part
+        self.Tlink = []  # Transforms for each link (const)
+        self.Tjoint = []  # Transforms for each joint (init eye)
+        self.Tcurr = []  # Coordinate frame of current (init eye)
+        for i in range(len(self.Rdesc)):
+            self.Tlink.append(rt.rpyxyz2H(self.Rdesc[i][0:3], self.Rdesc[i][3:6]))
+            self.Tcurr.append([[1, 0, 0, 0], [0, 1, 0, 0], [0, 0, 1, 0.], [0, 0, 0, 1]])
+            self.Tjoint.append([[1, 0, 0, 0], [0, 1, 0, 0], [0, 0, 1, 0.], [0, 0, 0, 1]])
 
-		self.Tlinkzero=rt.rpyxyz2H(self.Rdesc[0][0:3],self.Rdesc[0][3:6])
-		self.Tlink[0]=np.matmul(self.Tbase,self.Tlink[0])
+        self.Tlinkzero = rt.rpyxyz2H(self.Rdesc[0][0:3], self.Rdesc[0][3:6])
+        self.Tlink[0] = np.matmul(self.Tbase, self.Tlink[0])
 
-		# initialize Jacobian matrix
-		self.J=np.zeros((6,5))
+        # initialize Jacobian matrix
+        self.J = np.zeros((6, 5))
 
-		self.q=[0.,0.,0.,0.,0.,0.]
-		self.ForwardKin([0.,0.,0.,0.,0.])
+        self.q = [0., 0., 0., 0., 0., 0.]
+        self.ForwardKin([0., 0., 0., 0., 0.])
 
-	def ForwardKin(self,ang):
-		'''
-		inputs: joint angles
-		outputs: joint transforms for each joint, Jacobian matrix
-		'''
-		self.q[0:-1]=ang
-		# TODO: implement your Forward Kinematics here
-		for i in range(len(self.q)):
-			self.Tjoint[i] = [[math.cos(self.q[i]), -math.sin(self.q[i]), 0, 0],
-							  [math.sin(self.q[i]), math.cos(self.q[i]), 0, 0],
-							  [0, 0, 1, 0],
-							  [0, 0, 0, 1]]
+    def ForwardKin(self, ang):
+        '''
+        inputs: joint angles
+        outputs: joint transforms for each joint, Jacobian matrix
+        '''
+        self.q[0:-1] = ang
+        # TODO: implement your Forward Kinematics here
+        # TODO: Compute current joint and end effector coordinate frames (self.Tjoint). Remember that not all joints rotate about the z axis!
 
-			if i == 0:
-				self.Tcurr[i] = np.matmul(self.Tlink[i], self.Tjoint[i])
-			else:
-				self.Tcurr[i] = np.matmul(np.matmul(self.Tcurr[i-1], self.Tlink[i]), self.Tjoint[i])
+        for i in range(len(self.q)):
+            self.Tjoint[i] = rt.rpyxyz2H(np.array(self.axis[i]) * self.q[i], [0, 0, 0])
 
-		# TODO: Compute current joint and end effector coordinate frames (self.Tjoint). Remember that not all joints rotate about the z axis!
+            # z-axis only example
+            # self.Tjoint[i] = [[math.cos(self.q[i]), -math.sin(self.q[i]), 0, 0],
+            # 				  [math.sin(self.q[i]), math.cos(self.q[i]), 0, 0],
+            # 				  [0, 0, 1, 0],
+            # 				  [0, 0, 0, 1]]
 
-		# TODO: Compute Jacobian matrix
-		for i in range(len(self.Tcurr) - 1):
-			# Position of end effector - Position of ith joint
-			p = self.Tcurr[-1][0:3, 3] - self.Tcurr[i][0:3, 3] # From all 3 elements, return index 3
-			a = self.axis[i] # Define axis to use as joint axis
-			# a = self.Tcurr[i][0:3, 2]  # Lecture, about Z axis
-			self.J[0:3,i] = np.cross(a,p)
-			self.J[3:7, i] = a
-		return self.Tcurr, self.J
+            if i == 0:
+                self.Tcurr[i] = np.matmul(self.Tlink[i], self.Tjoint[i])
+            else:
+                self.Tcurr[i] = np.matmul(np.matmul(self.Tcurr[i - 1], self.Tlink[i]), self.Tjoint[i])
 
+        # TODO: Compute Jacobian matrix
+        for i in range(len(self.Tcurr) - 1):
+            # Position of end effector - Position of ith joint
+            p = self.Tcurr[-1][0:3, 3] - self.Tcurr[i][0:3, 3]  # From all 3 elements, return index 3
+            axis = np.argwhere(self.axis[i])[0][0]  # Define axis to use as joint axis
+            a = self.Tcurr[i][0:3, axis]  # Lecture, about xx axis
+            self.J[0:3, i] = np.cross(a, p)
+            self.J[3:7, i] = a
+        return self.Tcurr, self.J
 
-	# def IterInvKin(self,ang,TGoal):
-	# 	'''
-	# 	inputs: starting joint angles (ang), target end effector pose (TGoal)
-    #
-	# 	outputs: computed joint angles to achieve desired end effector pose,
-	# 	Error in your IK solution compared to the desired target
-	# 	'''
-	# 	self.ForwardKin(ang)
-    #
-	# 	Err=[0,0,0,0,0,0] # error in position and orientation, initialized to 0
-	# 	for s in range(10000):
-	# 		#TODO: Compute rotation error
-    #
-	# 		#TODO: Compute position error
-    #
-	# 		#TODO: Update joint angles
-    #
-	# 		#TODO: Recompute forward kinematics for new angles
-    #
-	# 	return self.q[0:-1], Err
+    def IterInvKin(self, ang, TGoal):
+        '''
+        inputs: starting joint angles (ang), target end effector pose (TGoal)
 
+        outputs: computed joint angles to achieve desired end effector pose,
+        Error in your IK solution compared to the desired target
+        '''
+        self.ForwardKin(ang)
 
-	def PlotSkeleton(self,ang):
-		'''
-		optional function: this code plots the output of you FK
-		'''
-		#Compute forward kinematics for ang
-		self.ForwardKin(ang)
+        Err = [0, 0, 0, 0, 0, 0]  # error in position and orientation, initialized to 0
+        for s in range(10000):
+            pass
+        # TODO: Compute rotation error
 
-		#Create figure
-		fig =plt.figure()
-		ax = fig.add_subplot(111,projection='3d')
+        # TODO: Compute position error
 
-		#Draw links along coordinate frames
-		for i in range(len(self.Tcurr)):
-			ax.scatter(self.Tcurr[i][0,3], self.Tcurr[i][1,3], self.Tcurr[i][2,3], c='k', marker='.')
-			if i is 0:
-				ax.plot([0,self.Tcurr[i][0,3]], [0,self.Tcurr[i][1,3]], [0,self.Tcurr[i][2,3]], c='b')
-			else:
-				ax.plot([self.Tcurr[i-1][0,3],self.Tcurr[i][0,3]], [self.Tcurr[i-1][1,3],self.Tcurr[i][1,3]], [self.Tcurr[i-1][2,3],self.Tcurr[i][2,3]], c='k')
+        # TODO: Update joint angles
 
-		#Format axes and display
-		ax.axis('equal')
-		ax.set(xlim=(-1., 1.), ylim=(-1., 1.), zlim=(0,1.))
-		ax.set_xlabel('X-axis')
-		ax.set_ylabel('Y-axis')
-		ax.set_zlabel('Z-axis')
+        # TODO: Recompute forward kinematics for new angles
 
-		plt.show()
+        return self.q[0:-1], Err
 
+    def PlotSkeleton(self, ang):
+        '''
+        optional function: this code plots the output of you FK
+        '''
+        # Compute forward kinematics for ang
+        self.ForwardKin(ang)
 
+        # Create figure
+        fig = plt.figure()
+        ax = fig.add_subplot(111, projection='3d')
+
+        # Draw links along coordinate frames
+        for i in range(len(self.Tcurr)):
+            ax.scatter(self.Tcurr[i][0, 3], self.Tcurr[i][1, 3], self.Tcurr[i][2, 3], c='k', marker='.')
+            if i is 0:
+                ax.plot([0, self.Tcurr[i][0, 3]], [0, self.Tcurr[i][1, 3]], [0, self.Tcurr[i][2, 3]], c='b')
+            else:
+                ax.plot([self.Tcurr[i - 1][0, 3], self.Tcurr[i][0, 3]], [self.Tcurr[i - 1][1, 3], self.Tcurr[i][1, 3]],
+                        [self.Tcurr[i - 1][2, 3], self.Tcurr[i][2, 3]], c='k')
+
+        # Format axes and display
+        ax.axis('equal')
+        ax.set(xlim=(-1., 1.), ylim=(-1., 1.), zlim=(0, 1.))
+        ax.set_xlabel('X-axis')
+        ax.set_ylabel('Y-axis')
+        ax.set_zlabel('Z-axis')
+
+        plt.show()
